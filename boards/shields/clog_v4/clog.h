54000ad70dc3060a27f41d17bc49e4b81771105a
Tarik Kirgin <mail@tarikkirgin.com>
2025-12-26 23:10:10 +0000

    fix and use key labels

--- boards/shields/clog_v4/clog.h
+++ boards/shields/clog_v4/clog.h
@@ -2,9 +2,9 @@
 
            ╭───────────────┬────────────────╮                   ╭─────────────────┬─────────────────╮      
   ╭────────╯ 0   1   2   3 │  4   5   6   7 ╰───────╮   ╭───────╯ LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 ╰───────╮
-  │  8   9  10  11  12  13 │ 15  16  17  18  19  20 │   │ LM5 LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 RM5 │
-  ╰───╮ 21  22  23  24  25 │ 26  27  28  29  30 ╭───╯   ╰───╮ LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 ╭───╯
-      ╰───────────╮ 31  32 │ 33  34 ╭───────────╯           ╰───────────╮ LH1 LH0 │ RH0 RH1 ╭───────────╯    
+  │  8   9  10  11  12  13 │ 14  15  16  17  18  19 │   │ LM5 LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 RM5 │
+  ╰───╮ 20  21  22  23  24 │ 25  26  27  28  29 ╭───╯   ╰───╮ LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 ╭───╯
+      ╰───────────╮ 30  31 │ 32  33 ╭───────────╯           ╰───────────╮ LH1 LH0 │ RH0 RH1 ╭───────────╯    
                   ╰────────┴────────╯                                   ╰─────────┴─────────╯              */ 
 
 #pragma once
@@ -33,20 +33,20 @@
 #define RM4 18
 #define RM5 19
 
-#define LB0 25  // left-bottom row
-#define LB1 24
-#define LB2 23
-#define LB3 22
-#define LB4 21
+#define LB0 24  // left-bottom row
+#define LB1 23
+#define LB2 22
+#define LB3 21
+#define LB4 20
 
-#define RB0 26  // right-bottom row
-#define RB1 27
-#define RB2 28
-#define RB3 29
-#define RB4 30
+#define RB0 25  // right-bottom row
+#define RB1 26
+#define RB2 27
+#define RB3 28
+#define RB4 29
 
-#define LH0 32  // left thumb keys
-#define LH1 31
+#define LH0 31  // left thumb keys
+#define LH1 30
 
-#define RH0 33  // right thumb keys
-#define RH1 34
+#define RH0 32  // right thumb keys
+#define RH1 33
